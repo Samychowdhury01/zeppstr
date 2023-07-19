188e60b2f5a401f48f3160b6c7e98e5820d54b66
samychowdhury01 <samychowdhury84@gmail.com>
2023-07-19 14:50:58 +0600

    added footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import logo from "../../assets/home/logo.png";
+import postImg from "../../assets/home/post-img.png";
+import { FaFacebook, FaTwitter, FaLinkedin, FaYoutube } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="gradient-bg p-10">
+      <footer className="footer text-base-content">
+        <div>
+          <img src={logo} alt="logo" />
+          <p>
+            FindBestSEO has ranked PageTraffic <br /> among the top 10 SEO
+            companies for 2020.
+          </p>
+          <div className="text-4xl text-secondary flex gap-10 mt-10">
+            <FaFacebook />
+            <FaLinkedin />
+            <FaTwitter />
+            <FaYoutube />
+          </div>
+        </div>
+        <div>
+          <span className="footer-title text-secondary text-2xl font-semibold">
+            Company
+          </span>
+          <a className="link link-hover">About</a>
+          <a className="link link-hover">Services</a>
+          <a className="link link-hover">Press</a>
+          <a className="link link-hover">Resources</a>
+          <a className="link link-hover">Blog</a>
+        </div>
+        <div>
+          <span className="footer-title text-secondary text-2xl font-semibold">
+            Latest news
+          </span>
+          <div>
+            <div className="flex gap-5 mb-5">
+              <img src={postImg} alt="new-post" />
+              <div>
+              <h4 className="text-secondary text-lg font-medium mb-2">Per Click Management</h4>
+              <p className="text-xs">PageTraffic is a premier Search Engine Marketing ( SEM).</p>
+              </div>
+            </div>
+            <div className="flex gap-5 mb-5">
+              <img src={postImg} alt="new-post" />
+              <div>
+              <h4 className="text-secondary text-lg font-medium mb-2">Per Click Management</h4>
+              <p className="text-xs">PageTraffic is a premier Search Engine Marketing ( SEM).</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <div className="flex justify-between mt-20">
+        <p>
+          <span>&copy;</span>
+          2023 Zeppstr. Patent Pending. All Rights Reserved.
+        </p>
+        <p>Privacy | Terms of Service</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      textColor:{
+        primary: '#E71D36',
+        secondary: '#2EC4B6'
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
