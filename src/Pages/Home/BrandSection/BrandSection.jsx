a59612c00e17a813da73d106a3392cf9ac8edd00
samychowdhury01 <samychowdhury84@gmail.com>
2023-07-22 17:42:39 +0600

    updated features section

--- src/Pages/Home/BrandSection/BrandSection.jsx
+++ src/Pages/Home/BrandSection/BrandSection.jsx
@@ -8,7 +8,7 @@ import brand5 from "../../../assets/home/image-5.png";
 const BrandSection = () => {
   return (
     <section className="my-16 px-8 md:px-28">
-      <h1 className="text-center text-2xl md:text-4xl font-bold mb-6">Global Brands that have trusted us</h1>
+      <h1 className="text-center text-2xl text-black md:text-4xl font-bold mb-6">Global Brands that have trusted us</h1>
       <div className="gradient-bg flex flex-col md:flex-row items-center justify-center gap-10 rounded-xl shadow-xl px-10 py-4">
         <img src={brand2} alt="brand-image" />
         <img src={brand4} alt="brand-image" />
--- src/Pages/Home/FeatureSection/FeatureSection.jsx
+++ src/Pages/Home/FeatureSection/FeatureSection.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import {BiPen} from 'react-icons/bi'
-import {FaMagnifyingGlassChart} from 'react-icons/fa6'
+import { BiPen } from "react-icons/bi";
+import { FaMagnifyingGlassChart } from "react-icons/fa6";
+import Button from "../../../Components/Button/Button";
 
 const FeatureSection = () => {
   return (
     <section className="mb-16 px-8 md:px-28">
       <div className="flex flex-col md:flex-row items-center gap-28">
         {/* text container */}
-        <div className="space-y-[38px]">
+        <div className="space-y-[38px] text-black">
           <h2 className="text-3xl font-medium">
-            What do we offer you as an <span className="text-primary">SEO Agency?</span>
+            What do we offer you as an{" "}
+            <span className="text-primary">SEO Agency?</span>
           </h2>
           <p>
             Nowadays, SEO optimization is an indispensable element for
@@ -23,54 +25,66 @@ const FeatureSection = () => {
           <div className="flex gap-12">
             <div>
               <h3 className="text-3xl font-medium text-primary">1.9k</h3>
-              <p className="text-lg">Total Audience</p>
+              <p className="text-lg text-black">Total Audience</p>
             </div>
             <div>
               <h3 className="text-3xl font-medium text-secondary">5.7k</h3>
-              <p className="text-lg">Total Audience</p>
+              <p className="text-lg text-black">Total Audience</p>
             </div>
           </div>
-          <button className="btn secondary-btn hover:text-black">Explore Now</button>
+          <Button>Explore Now</Button>
         </div>
         {/* icon container */}
         <div className="flex flex-col md:flex-row md:gap-5">
           {/* left side */}
           <div>
-
-            <div className="border border-[#E71D36] rounded-3xl p-5 space-y-3 mb-8 md:w-[270px]">
-            <div className="w-[100px] h-[100px] bg-[#FFECEA] flex items-center justify-center rounded-xl">
-             <BiPen className="text-primary text-5xl"/>
-            </div>
-            <h4 className="text-2xl font-medium">Global SEO</h4>
-            <p>We combine creative SEO tactics, enhanced marketing techniques, and high-end inbound content marketing to help businesses rank in the targeted country.</p>
+            <div className="border bg-white border-[#E71D36] rounded-3xl p-5 space-y-3 mb-8 md:w-[270px]">
+              <div className="w-[100px] h-[100px] bg-[#FFECEA] flex items-center justify-center rounded-xl">
+                <BiPen className="text-primary text-5xl" />
+              </div>
+              <h4 className="text-2xl font-medium text-black">Global SEO</h4>
+              <p className="text-black">
+                We combine creative SEO tactics, enhanced marketing techniques,
+                and high-end inbound content marketing to help businesses rank
+                in the targeted country.
+              </p>
             </div>
             <div className="bg-[#F6FFFE] border border-[#2EC4B6] rounded-3xl p-5 space-y-3 mb-8 md:w-[270px]">
-            <div className="w-[100px] h-[100px] bg-[#D2FFFB] flex items-center justify-center rounded-xl">
-             <FaMagnifyingGlassChart className="text-secondary text-5xl"/>
+              <div className="w-[100px] h-[100px] bg-[#D2FFFB] flex items-center justify-center rounded-xl">
+                <FaMagnifyingGlassChart className="text-secondary text-5xl" />
+              </div>
+              <h4 className="text-2xl font-medium text-black">Global SEO</h4>
+              <p className="text-black">
+                We combine creative SEO tactics, enhanced marketing techniques,
+                and high-end inbound content marketing to help businesses rank
+                in the targeted country.
+              </p>
             </div>
-            <h4 className="text-2xl font-medium">Global SEO</h4>
-            <p>We combine creative SEO tactics, enhanced marketing techniques, and high-end inbound content marketing to help businesses rank in the targeted country.</p>
-            </div>
-
           </div>
           {/* right side */}
           <div className="md:mt-[100px]">
-
             <div className="bg-[#F6FFFE] border border-[#2EC4B6] rounded-3xl p-5 space-y-3 mb-8 md:w-[270px]">
-            <div className="w-[100px] h-[100px] bg-[#D2FFFB] flex items-center justify-center rounded-xl">
-             <FaMagnifyingGlassChart className="text-secondary text-5xl"/>
+              <div className="w-[100px] h-[100px] bg-[#D2FFFB] flex items-center justify-center rounded-xl">
+                <FaMagnifyingGlassChart className="text-secondary text-5xl" />
+              </div>
+              <h4 className="text-2xl font-medium text-black">Global SEO</h4>
+              <p className="text-black">
+                We combine creative SEO tactics, enhanced marketing techniques,
+                and high-end inbound content marketing to help businesses rank
+                in the targeted country.
+              </p>
             </div>
-            <h4 className="text-2xl font-medium">Global SEO</h4>
-            <p>We combine creative SEO tactics, enhanced marketing techniques, and high-end inbound content marketing to help businesses rank in the targeted country.</p>
+            <div className="border border-[#E71D36] bg-white rounded-3xl p-5 space-y-3 md:w-[270px]">
+              <div className="w-[100px] h-[100px] bg-[#FFECEA] flex items-center justify-center rounded-xl">
+                <BiPen className="text-primary text-5xl" />
+              </div>
+              <h4 className="text-2xl font-medium text-black">Global SEO</h4>
+              <p className="text-black">
+                We combine creative SEO tactics, enhanced marketing techniques,
+                and high-end inbound content marketing to help businesses rank
+                in the targeted country.
+              </p>
             </div>
-            <div className="border border-[#E71D36] rounded-3xl p-5 space-y-3 md:w-[270px]">
-            <div className="w-[100px] h-[100px] bg-[#FFECEA] flex items-center justify-center rounded-xl">
-             <BiPen className="text-primary text-5xl"/>
-            </div>
-            <h4 className="text-2xl font-medium">Global SEO</h4>
-            <p>We combine creative SEO tactics, enhanced marketing techniques, and high-end inbound content marketing to help businesses rank in the targeted country.</p>
-            </div>
-
           </div>
         </div>
       </div>
